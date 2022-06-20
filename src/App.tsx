10b1b1c1fb4cb5fbecec741d126cc4f8d0da689d
ibrokhim <mr017programmer@gmail.com>
2022-06-20 17:13:43 +0500

    formState

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,53 @@ import { Header } from "./components/Header";
 let renderCount = 0;
 
 type FromValues = {
-  firstName: string;
-  lastName: string;
-  number: number;
+  yourDetails: {
+    firstName: string;
+    lastName: string;
+    number: number;
+  };
 };
 
 function App() {
-  const { register, handleSubmit, watch } = useForm<FromValues>({
-    mode: "all",
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: {
+      isDirty,
+      dirtyFields,
+      isSubmitted,
+      isSubmitting,
+      isSubmitSuccessful,
+      submitCount,
+      errors,
+      isValid,
+      isValidating,
+      touchedFields,
+    },
+  } = useForm<FromValues>({
+    mode: "onChange",
     // delayError: 1,
     defaultValues: {
-      firstName: "Ibrokhim",
-      lastName: "Ismoilov",
-      number: 0,
+      yourDetails: {
+        firstName: "",
+        lastName: "",
+        number: 0,
+      },
     },
   });
 
+  console.log("isDirty ", isDirty); // fieldlarning qiymati bormi
+  console.log("dirtyFields ", dirtyFields); // qiymati bor fieldlar
+  console.log("isSubmitted ", isSubmitted); // forma submit bo'ldimi?
+  console.log("isSubmitSuccessful ", isSubmitSuccessful); // forma muvofaqqiyatli submit bo'ldimi?
+  console.log("submitCount ", submitCount); // forma necha marotaba submit bo'lgani.
+  console.log("isSubmitting ", isSubmitting); // forma submit bo'lmoqdami?
+  console.log("errors ", errors); // fieldlardagi hatoliklar.
+  console.log("isValid ", isValid); // submit bo'lganida validatsiya true/false.
+  console.log("isValidating	", isValidating); // submit bo'layotganda validatsiya true/false.
+  console.log("touchedFields ", touchedFields); // tanlangan fielddan blur bo'lgan holati.
   renderCount++;
 
   useEffect(() => {
@@ -29,15 +60,21 @@ function App() {
     return () => subscriptsion.unsubscribe();
   }, [watch]);
 
-  const { firstName } = watch();
+  const {
+    yourDetails: { firstName },
+  } = watch();
+
+  const onSubmit = async (data: FromValues) => {
+    // await setTimeout(() => console.log(data), 3000);
+  };
 
   return (
     <div className="App">
       <Header renderCount={renderCount} title="React-hook-form" />
-      <form onSubmit={handleSubmit((data) => console.log("Submiting", data))}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <p>FirstName: {firstName}</p>
         <input
-          {...register("firstName", {
+          {...register("yourDetails.firstName", {
             required: {
               value: true,
               message: "Majburiy",
@@ -46,7 +83,7 @@ function App() {
           })}
         />
         <input
-          {...register("lastName", {
+          {...register("yourDetails.lastName", {
             required: {
               value: true,
               message: "Majburiy",
@@ -57,11 +94,21 @@ function App() {
 
         <input
           type="number"
-          {...register("number", {
+          {...register("yourDetails.number", {
             valueAsNumber: true,
             required: true,
           })}
         />
+
+        <button
+          onClick={() => {
+            setValue("yourDetails.firstName", "Ibrokhim", {
+              shouldTouch: true,
+            });
+          }}
+        >
+          setValue
+        </button>
 
         <input type="submit" />
       </form>
