425e383a715b1f05698276e6ca8b99aa62f4aceb
ibrokhim <mr017programmer@gmail.com>
2022-08-14 14:50:34 +0500

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -14,46 +14,41 @@ type FromValues = {
 };
 
 function App() {
+  renderCount++;
+
   const {
     register,
     handleSubmit,
     watch,
     setValue,
-    formState: {
-      isDirty,
-      dirtyFields,
-      isSubmitted,
-      isSubmitting,
-      isSubmitSuccessful,
-      submitCount,
-      errors,
-      isValid,
-      isValidating,
-      touchedFields,
-    },
+    formState,
+    reset,
+    getValues,
   } = useForm<FromValues>({
     mode: "onChange",
     // delayError: 1,
     defaultValues: {
       yourDetails: {
-        firstName: "",
-        lastName: "",
+        firstName: "asd",
+        lastName: "asd",
         number: 0,
       },
     },
   });
 
-  console.log("isDirty ", isDirty); // fieldlarning qiymati bormi
-  console.log("dirtyFields ", dirtyFields); // qiymati bor fieldlar
-  console.log("isSubmitted ", isSubmitted); // forma submit bo'ldimi?
-  console.log("isSubmitSuccessful ", isSubmitSuccessful); // forma muvofaqqiyatli submit bo'ldimi?
-  console.log("submitCount ", submitCount); // forma necha marotaba submit bo'lgani.
-  console.log("isSubmitting ", isSubmitting); // forma submit bo'lmoqdami?
-  console.log("errors ", errors); // fieldlardagi hatoliklar.
-  console.log("isValid ", isValid); // submit bo'lganida validatsiya true/false.
-  console.log("isValidating	", isValidating); // submit bo'layotganda validatsiya true/false.
-  console.log("touchedFields ", touchedFields); // tanlangan fielddan blur bo'lgan holati.
-  renderCount++;
+  // console.log("isDirty ", isDirty); // fieldlarning qiymati bormi
+  // console.log("dirtyFields ", dirtyFields); // qiymati bor fieldlar
+  // console.log("isSubmitted ", isSubmitted); // forma submit bo'ldimi?
+  // console.log("isSubmitSuccessful ", isSubmitSuccessful); // forma muvofaqqiyatli submit bo'ldimi?
+  // console.log("submitCount ", submitCount); // forma necha marotaba submit bo'lgani.
+  // console.log("isSubmitting ", isSubmitting); // forma submit bo'lmoqdami?
+  // console.log("errors ", errors); // fieldlardagi hatoliklar.
+  // console.log("isValid ", isValid); // submit bo'lganida validatsiya true/false.
+  // console.log("isValidating	", isValidating); // submit bo'layotganda validatsiya true/false.
+
+  // useEffect(() => {
+  //   console.log("touchedFields ", formState.touchedFields); // tanlangan fielddan blur bo'lgan holati.
+  // }, [formState]);
 
   useEffect(() => {
     const subscriptsion = watch((data) => console.log(data));
@@ -67,6 +62,22 @@ function App() {
   const onSubmit = async (data: FromValues) => {
     // await setTimeout(() => console.log(data), 3000);
   };
+
+  useEffect(() => {
+    if (formState.isSubmitSuccessful) {
+      reset(
+        {
+          ...getValues(),
+          yourDetails: { ...getValues().yourDetails, firstName: "Ibrokhim" },
+        },
+        {
+          keepDefaultValues: true,
+        }
+      );
+    }
+
+    // eslint-disable-next-line
+  }, [formState, reset]);
 
   return (
     <div className="App">
@@ -108,6 +119,25 @@ function App() {
           }}
         >
           setValue
+        </button>
+
+        <button
+          onClick={() =>
+            reset(
+              {
+                yourDetails: {
+                  firstName: "Ibrokhim",
+                  lastName: "Ismoilov",
+                  number: 23,
+                },
+              },
+              {
+                keepDefaultValues: true,
+              }
+            )
+          }
+        >
+          reset
         </button>
 
         <input type="submit" />
